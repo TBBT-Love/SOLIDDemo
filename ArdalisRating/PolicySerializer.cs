6d7d3a1ab57bd61648876de46808765c8cf00f90
indu.bala <indu.bala@ihsmarkit.com>
2025-01-29 01:57:45 +0530

    Add project files.

---
+++ ArdalisRating/PolicySerializer.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Converters;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArdalisRating
+{
+    public class PolicySerializer
+    {
+        public Policy GetPolicyfromJSONString(string inputPolicyJson)
+        {
+            return JsonConvert.DeserializeObject<Policy>(inputPolicyJson,
+                new StringEnumConverter());
+        }
+    }
+}
